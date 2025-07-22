369a826700f814befa569102373869bff771c375
witwar <witwar@yandex.ru>
2025-07-22 08:18:18 +0300

    Новый релиз

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-tokens',
-    version='0.2.0',
+    version='0.3.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
